f4bc00e1f4fcb4e0c5e64ce634a1558e2474b0a5
Jimecc <1813085460@qq.com>
2022-06-21 18:02:32 +0800

    Init

---
+++ kkcd/src/main/java/com/jim/kkcd/KkcdApplication.java
@@ -0,0 +1,13 @@
+package com.jim.kkcd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KkcdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KkcdApplication.class, args);
+    }
+
+}
